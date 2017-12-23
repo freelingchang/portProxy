c3978590d157b4d3d2137f112c040a3cb5554053
freelingchang <freelingchang@gmail.com>
2017-12-23 14:37:19 +0800

    fix tab

--- client.go
+++ client.go
@@ -7,31 +7,31 @@ import (
 	"io"
 )
 
-func main(){
+func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	service := "127.0.0.1:33128"
 	localSrv := "172.16.0.101:80"
-	clients := make(chan net.Conn,20)
-	clientNum := make(chan int,20)
-	createChan(5,clientNum)
-	go clientHandle(clients,localSrv,clientNum)
-	keepConnectNum(service,clientNum,clients)
+	clients := make(chan net.Conn, 20)
+	clientNum := make(chan int, 20)
+	createChan(5, clientNum)
+	go clientHandle(clients, localSrv, clientNum)
+	keepConnectNum(service, clientNum, clients)
 
 }
 
-func clientHandle(clients chan net.Conn,localSrv string,clientNum chan int){
+func clientHandle(clients chan net.Conn, localSrv string, clientNum chan int) {
 	for {
 		client := <-clients
 		go forwardLocal(client, localSrv, clientNum)
 	}
 }
 
-func forwardLocal(client net.Conn,localSvc string,clientNum chan int){
+func forwardLocal(client net.Conn, localSvc string, clientNum chan int) {
 	defer insertNum(clientNum)
-	buff := make([]byte,4096)
-	n,err := client.Read(buff)
+	buff := make([]byte, 4096)
+	n, err := client.Read(buff)
 	if err != nil {
-		log.Println("第一次读取数据失败",err)
+		log.Println("第一次读取数据失败", err)
 		time.Sleep(10 * time.Second)
 		return
 	}
@@ -42,53 +42,52 @@ func forwardLocal(client net.Conn,localSvc string,clientNum chan int){
 		time.Sleep(10 * time.Second)
 		return
 	}
-	n,err = conn.Write(buff[0:n])
+	n, err = conn.Write(buff[0:n])
 	if err != nil {
 		log.Println("转发错误")
 		return
 	}
-	go fdCopy(client,conn)
-	go fdCopy(conn,client)
+	go fdCopy(client, conn)
+	go fdCopy(conn, client)
 }
 
-func insertNum(clientNum chan int){
+func insertNum(clientNum chan int) {
 	clientNum <- 1
 }
 
-
-func createChan(n int,number chan int){
-	for i:=0;i<n;i++ {
+func createChan(n int, number chan int) {
+	for i := 0; i < n; i++ {
 		number <- 1
-		}
-		log.Println("初始化chan int")
+	}
+	log.Println("初始化chan int")
 }
 
-func connect(clientIP string,clients chan net.Conn,clientNum chan int)  {
+func connect(clientIP string, clients chan net.Conn, clientNum chan int) {
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", clientIP)
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	if err != nil {
 		time.Sleep(10 * time.Second)
 		clientNum <- 1
-		log.Println("连接失败",clientIP)
+		log.Println("连接失败", clientIP)
 		return
 	}
 	clients <- conn
-	log.Println("建立连接成功",conn.LocalAddr())
+	log.Println("建立连接成功", conn.LocalAddr())
 }
 
-func keepConnectNum(clientIP string,num chan int,clients chan net.Conn){
+func keepConnectNum(clientIP string, num chan int, clients chan net.Conn) {
 	for {
 		select {
-		case _ = <- num:
-			go connect(clientIP,clients,num)
+		case _ = <-num:
+			go connect(clientIP, clients, num)
 		}
 	}
 }
 
-func fdCopy(f net.Conn,t net.Conn)  {
+func fdCopy(f net.Conn, t net.Conn) {
 	defer f.Close()
 	defer t.Close()
-	_,err := io.Copy(f,t)
+	_, err := io.Copy(f, t)
 	if err != nil {
 		//log.Println(err)
 	}
@@ -117,7 +116,7 @@ func checkConn(client net.Conn) bool {
 	if _, err := client.Read(one); err == io.EOF {
 		log.Println("连接已关闭")
 		return true
-	}else {
+	} else {
 		client.SetReadDeadline(time.Time{})
 		return true
 	}
--- server.go
+++ server.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-func main(){
+func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	service := ":33128"
 	clientIP := "127.0.0.1"
@@ -25,12 +25,12 @@ func main(){
 			log.Println("建立连接错误")
 			continue
 		}
-		go Handle(conn,clients,clientIP)
+		go Handle(conn, clients, clientIP)
 	}
 }
 
-func Handle(conn net.Conn,clients chan net.Conn,clientIP string){
-	ip := strings.Split(conn.RemoteAddr().String(),":")
+func Handle(conn net.Conn, clients chan net.Conn, clientIP string) {
+	ip := strings.Split(conn.RemoteAddr().String(), ":")
 	if len(ip) < 2 {
 		defer conn.Close()
 		log.Println("客户端IP错误")
@@ -41,25 +41,25 @@ func Handle(conn net.Conn,clients chan net.Conn,clientIP string){
 		clients <- conn
 		return
 	}
-	one := make([]byte,1024)
-	n,err := conn.Read(one)
+	one := make([]byte, 1024)
+	n, err := conn.Read(one)
 	if err != nil {
 		return
 	}
-	client ,buff := getConn(clients,one[0:n])
-	_,err = conn.Write(buff)
+	client, buff := getConn(clients, one[0:n])
+	_, err = conn.Write(buff)
 	if err != nil {
 		log.Println("建立连接失败")
 	}
 
-	log.Println("双向连接建立，开始转发",client.RemoteAddr())
+	log.Println("双向连接建立，开始转发", client.RemoteAddr())
 	go fdCopy(client, conn)
 	go fdCopy(conn, client)
 }
 
-func getConn(clients chan net.Conn,one []byte) (net.Conn,[]byte){
+func getConn(clients chan net.Conn, one []byte) (net.Conn, []byte) {
 	for {
-		client := <- clients
+		client := <-clients
 		client = getConnWrite(client, one)
 		if client == nil {
 			continue
@@ -68,31 +68,31 @@ func getConn(clients chan net.Conn,one []byte) (net.Conn,[]byte){
 		client, buf := getConnRead(client, buff)
 		if client == nil {
 			continue
-		}else{
+		} else {
 			return client, buf
 		}
 	}
 
 }
 
-func getConnRead(client net.Conn,one []byte) (net.Conn,[]byte) {
+func getConnRead(client net.Conn, one []byte) (net.Conn, []byte) {
 	client.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
 	n, err := client.Read(one)
 	if err != nil {
 		log.Println("连接已关闭")
-		return nil,nil
+		return nil, nil
 	}
 	if n != len(one) {
 		log.Println("连接已关闭")
-		return nil,nil
+		return nil, nil
 	} else {
 		log.Println("测试连接成功")
 		client.SetReadDeadline(time.Time{})
-		return client,one[0:n]
+		return client, one[0:n]
 	}
 }
 
-func getConnWrite(client  net.Conn,one []byte) (net.Conn) {
+func getConnWrite(client net.Conn, one []byte) (net.Conn) {
 	client.SetWriteDeadline(time.Now().Add(100 * time.Millisecond))
 	n, err := client.Write(one)
 	if err != nil {
@@ -109,10 +109,10 @@ func getConnWrite(client  net.Conn,one []byte) (net.Conn) {
 	}
 }
 
-func fdCopy(f net.Conn,t net.Conn)  {
+func fdCopy(f net.Conn, t net.Conn) {
 	defer f.Close()
 	defer t.Close()
-	_,err := io.Copy(f,t)
+	_, err := io.Copy(f, t)
 	if err != nil {
 		//log.Println(err)
 	}
