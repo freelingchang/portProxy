2f32813751ec4af3d67ebe98f8b8c4e87da986fd
freelingchang <freelingchang@gmail.com>
2017-12-27 18:37:01 +0800

    重构了一下

--- client.go
+++ client.go
@@ -5,50 +5,82 @@ import (
     "log"
     "time"
     "io"
+    "strings"
+    "fmt"
+    "os"
 )
 
 func main() {
     log.SetFlags(log.Lshortfile | log.LstdFlags)
-    service := "127.0.0.1:33128"
-    localSrv := "172.16.0.101:80"
-    clients := make(chan net.Conn, 20)
-    clientNum := make(chan int, 20)
-    createChan(5, clientNum)
-    go clientHandle(clients, localSrv, clientNum)
-    keepConnectNum(service, clientNum, clients)
+    service := "192.168.1.230:33128"
+    controlService := "192.168.1.230:33129"
+    localSrv := "192.168.1.230:22"
+    //clients := make(chan net.Conn, 100)
+    controlAlive := make(chan int ,1)
+    clientNum := make(chan int, 10)
+    go clientHandle(service, localSrv, clientNum)
+    //go ConnectForwardServer(service,,clients)
+    controlAlive <- 1
+    keepControlAlive(controlService, controlAlive,clientNum)
 
 }
 
-func clientHandle(clients chan net.Conn, localSrv string, clientNum chan int) {
+func clientHandle(service, localSrv string, clientNum chan int) {
     for {
-        client := <-clients
-        go forwardLocal(client, localSrv, clientNum)
+        select {
+        case _ = <-clientNum:
+            log.Println("开始建立转发隧道")
+            go forwardLocal(service, localSrv, clientNum)
+        }
     }
 }
 
-func forwardLocal(client net.Conn, localSvc string, clientNum chan int) {
-    defer insertNum(clientNum)
-    buff := make([]byte, 4096)
-    n, err := client.Read(buff)
-    if err != nil {
-        log.Println("第一次读取数据失败", err)
-        time.Sleep(10 * time.Second)
-        return
+func ConnectForwardServer(service string,clientNum chan int,clients chan net.Conn){
+    for {
+        select {
+        case _ = <-clientNum:
+            tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+            if err != nil {
+                fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+                os.Exit(1)
+            }
+            conn, err := net.DialTCP("tcp", nil, tcpAddr)
+            if err != nil {
+                log.Println("连接服务器失败",service)
+                time.Sleep(10 * time.Second)
+                clientNum <- 1
+                return
+            }
+            clients <- conn
+        }
     }
-    //log.Println(string(buff[0:n]))
+
+}
+
+
+func forwardLocal(service string,localSvc string, clientNum chan int) {
     tcpAddr, err := net.ResolveTCPAddr("tcp4", localSvc)
     conn, err := net.DialTCP("tcp", nil, tcpAddr)
     if err != nil {
         time.Sleep(10 * time.Second)
+        log.Println("连接本地服务器失败")
+        clientNum <- 1
+        defer conn.Close()
         return
     }
-    n, err = conn.Write(buff[0:n])
+    log.Println("连接本地服务器成功")
+    serviceTcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+    client, err := net.DialTCP("tcp", nil, serviceTcpAddr)
     if err != nil {
-        log.Println("转发错误")
+        time.Sleep(10 * time.Second)
+        log.Println("连接远程服务器失败")
+        clientNum <- 1
+        defer client.Close()
         return
     }
-    go fdCopy(client, conn)
-    go fdCopy(conn, client)
+    log.Println("连接远程服务器成功")
+    log.Println("开始转发")
+    copyDefer(conn,client)
 }
 
 func insertNum(clientNum chan int) {
@@ -62,62 +94,71 @@ func createChan(n int, number chan int) {
     log.Println("初始化chan int")
 }
 
-func connect(clientIP string, clients chan net.Conn, clientNum chan int) {
-    tcpAddr, err := net.ResolveTCPAddr("tcp4", clientIP)
+func recvControl(controlServer string, alive chan int, clientNum chan int) {
+    defer insertAlive(alive)
+    tcpAddr, err := net.ResolveTCPAddr("tcp4", controlServer)
     conn, err := net.DialTCP("tcp", nil, tcpAddr)
     if err != nil {
         time.Sleep(10 * time.Second)
-        clientNum <- 1
-        log.Println("连接失败", clientIP)
+        log.Println("连接失败", controlServer)
         return
     }
-    clients <- conn
-    log.Println("建立连接成功", conn.LocalAddr())
+    defer conn.Close()
+    log.Println("连接控制端口")
+    for {
+        buff := make([]byte,64)
+        n,err := conn.Read(buff)
+        if err != nil {
+        	log.Println("控制通道失效，关闭连接")
+        	return
+        }
+        log.Println("收到控制指令,长度为:",n)
+        content := string(buff[:n])
+        conLen := strings.Count(content,"\r\n")
+        conn.SetWriteDeadline(time.Now().Add(time.Second * 2))
+        _,err = conn.Write([]byte("ok\r\n"))
+        if err != nil {
+            log.Println("回复控制指令失败")
+            return
+        }
+        if conLen == 0 && n >0 {
+            log.Println("消息格式错误")
+            return
+        }
+        for i :=0;i<conLen;i++ {
+            log.Println("对端要求建立链接")
+            clientNum <- 1
+            log.Println("列队请求数加1")
+        }
+
+    }
 }
 
-func keepConnectNum(clientIP string, num chan int, clients chan net.Conn) {
+func insertAlive(alive chan int){
+	log.Println("重连")
+    alive <- 1
+}
+
+func keepControlAlive(controlServer string, alive chan int, clientNum chan int) {
     for {
         select {
-        case _ = <-num:
-            go connect(clientIP, clients, num)
+        case _ = <- alive:
+            go recvControl(controlServer, alive,clientNum)
         }
     }
 }
+
+func copyDefer(f net.Conn, t net.Conn) {
+    go fdCopy(f, t)
+    go fdCopy(t, f)
+}
+
 
 func fdCopy(f net.Conn, t net.Conn) {
     defer f.Close()
     defer t.Close()
     _, err := io.Copy(f, t)
     if err != nil {
-        //log.Println(err)
     }
 }
 
-func getConn(clients chan net.Conn) net.Conn {
-    for {
-        select {
-        case client := <-clients:
-            client.SetReadDeadline(time.Now())
-            one := []byte{}
-            if _, err := client.Read(one); err == io.EOF {
-                log.Println("连接已关闭")
-            } else {
-                client.SetReadDeadline(time.Time{})
-                return client
-            }
-        }
-    }
-}
-
-func checkConn(client net.Conn) bool {
-    return true
-    client.SetReadDeadline(time.Now())
-    one := []byte{}
-    if _, err := client.Read(one); err == io.EOF {
-        log.Println("连接已关闭")
-        return true
-    } else {
-        client.SetReadDeadline(time.Time{})
-        return true
-    }
-}
--- server.go
+++ server.go
@@ -13,108 +13,107 @@ import (
 func main() {
     log.SetFlags(log.Lshortfile | log.LstdFlags)
     service := ":33128"
-    clientIP := "127.0.0.1"
+    controlBind := ":33129"
+    clientIP := "192.168.1.3"
     tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
     checkError(err)
     listener, err := net.ListenTCP("tcp", tcpAddr)
     checkError(err)
-    clients := make(chan net.Conn)
+    clients := make(chan net.Conn,20)
+    controlChan := make(chan int,20)
+    go controlThread(controlBind,controlChan)
     for {
         conn, err := listener.Accept()
         if err != nil {
             log.Println("建立连接错误")
             continue
         }
-        go Handle(conn, clients, clientIP)
+        go Handle(conn, clients, clientIP,controlChan)
     }
 }
 
-func Handle(conn net.Conn, clients chan net.Conn, clientIP string) {
+func controlThread(bind string,n chan int)  {
+    tcpAddr, err := net.ResolveTCPAddr("tcp4", bind)
+    checkError(err)
+    listener, err := net.ListenTCP("tcp", tcpAddr)
+    checkError(err)
+    for {
+        conn, err := listener.Accept()
+        if err != nil {
+            log.Println("建立连接错误")
+            continue
+        }
+        log.Println("控制通道连接进入")
+        go controlHandle(conn, n)
+    }
+}
+func controlHandle(conn net.Conn,n chan int){
+    defer conn.Close()
+    for {
+        select {
+            case _ = <- n:
+                conn.SetWriteDeadline(time.Now().Add(time.Second * 2))
+                _, err := conn.Write([]byte("1\r\n"))
+                if err != nil {
+                    log.Println("控制消息发送失败")
+                    n <- 1
+                    return
+                }
+                conn.SetReadDeadline(time.Now().Add(time.Second * 2))
+                rep := make([]byte,64)
+                _, err = conn.Read(rep)
+                if err != nil {
+                    log.Println("控制消息发送收取回复失败")
+                    n <- 1
+                    return
+                }
+                log.Println("发送控制消息1次")
+        }
+    }
+
+}
+
+func Handle(conn net.Conn, clients chan net.Conn, clientIP string,controlChan chan int) {
     ip := strings.Split(conn.RemoteAddr().String(), ":")
     if len(ip) < 2 {
         defer conn.Close()
         log.Println("客户端IP错误")
         return
     }
+    if strings.HasPrefix(ip[0],"100") {
+        defer conn.Close()
+        log.Println("阿里云服务器,关闭连接")
+        return
+    }
     if ip[0] == clientIP {
         log.Println("client 发起连接,建立隧道")
         clients <- conn
         return
-    }
-    one := make([]byte, 1024)
-    n, err := conn.Read(one)
-    if err != nil {
-        return
-    }
-    client, buff := getConn(clients, one[0:n])
-    _, err = conn.Write(buff)
-    if err != nil {
-        log.Println("建立连接失败")
-    }
-
-    log.Println("双向连接建立，开始转发", client.RemoteAddr())
-    go fdCopy(client, conn)
-    go fdCopy(conn, client)
-}
-
-func getConn(clients chan net.Conn, one []byte) (net.Conn, []byte) {
-    for {
+    } else {
+        log.Println("一般客户端连接,开始转发", ip)
+        controlChan <- 1
         client := <-clients
-        client = getConnWrite(client, one)
-        if client == nil {
-            continue
-        }
-        buff := make([]byte, 1024)
-        client, buf := getConnRead(client, buff)
-        if client == nil {
-            continue
-        } else {
-            return client, buf
-        }
-    }
-
-}
-
-func getConnRead(client net.Conn, one []byte) (net.Conn, []byte) {
-    client.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
-    n, err := client.Read(one)
-    if err != nil {
-        log.Println("连接已关闭")
-        return nil, nil
-    }
-    if n != len(one) {
-        log.Println("连接已关闭")
-        return nil, nil
-    } else {
-        log.Println("测试连接成功")
-        client.SetReadDeadline(time.Time{})
-        return client, one[0:n]
+        go copyDefer(client, conn)
     }
 }
 
-func getConnWrite(client net.Conn, one []byte) (net.Conn) {
-    client.SetWriteDeadline(time.Now().Add(100 * time.Millisecond))
-    n, err := client.Write(one)
-    if err != nil {
-        log.Println("连接已关闭")
-        return nil
-    }
-    if n != len(one) {
-        log.Println("连接已关闭")
-        return nil
-    } else {
-        log.Println("测试连接成功")
-        client.SetWriteDeadline(time.Time{})
-        return client
-    }
+func copyDefer(f net.Conn, t net.Conn) {
+    defer f.Close()
+    defer t.Close()
+    c := make(chan int,2)
+    go fdCopy(f, t,c)
+    go fdCopy(t, f,c)
+    _ = <- c
+
 }
 
-func fdCopy(f net.Conn, t net.Conn) {
+
+func fdCopy(f net.Conn, t net.Conn,c chan int) {
     defer f.Close()
     defer t.Close()
     _, err := io.Copy(f, t)
     if err != nil {
-        //log.Println(err)
+        c <- 1
     }
 }
 
